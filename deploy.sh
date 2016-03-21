164f052b27a998d81b649e30cbf9c3171b85d491
Luca Valtulina <luca@speakup.nl>
2016-03-21 21:22:00 +0100

    Update deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 # configure ns-3
 ./waf distclean
-./waf configure ###add here further options if required
+./waf configure --disable-python ###add here further options if required
 
 
 # build ns-3
