dc54b9e5b29dbbe31a5963738e83f07434988493
Luca Valtulina <luca@speakup.nl>
2016-03-21 18:07:14 +0100

    Update deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
-# configure ns-3
+# build openflow switch support
+cd ofsid/
+./waf configure
+./waf build
+var=$(pwd)
+cd ../
+
+
+# configure ns-3 enabling openflow support
 ./waf distclean
-./waf configure ###add here further options if required
+./waf configure --with-openflow=$var ###add here further options if required
+cp -r ofsid/include/openflow/ build/
 
 
 # build ns-3
